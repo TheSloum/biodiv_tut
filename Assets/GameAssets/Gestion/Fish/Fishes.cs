3afd698b899e4b3bfaf22545a101e1866ceca3a1
Sakushi <116188905+ImSakushi@users.noreply.github.com>
2024-11-15 17:45:16 +0100

    fish button test

--- Assets/GameAssets/Gestion/Fish/Fishes.cs
+++ Assets/GameAssets/Gestion/Fish/Fishes.cs
@@ -2,29 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(fileName = "NewFish", menuName = "Fish")]
 public class Fishes : ScriptableObject
 {
     public int fishID = 0;
-
-    
     public string fishName = "Merlu";
-    public bool is_unlocked = true;
-
-    public void UnlockFish()
-    {
-        is_unlocked = true;
-    }
+    public bool is_unlocked = false; // Défini sur false par défaut
     public string fishDes = "Ce poisson a une grosse tête de con et il nage. \n En plus, il est pas bon.";
     public string fishOrigin = "Martinique du far-ouest";
     public string fishSize = "25m";
 
     public enum fishClasstypes
-{
-    vertébré,
-    invertébré,
-    miamousse
-}
+    {
+        Vertébré,
+        Invertébré,
+        Miamousse
+    }
     public fishClasstypes fishClass;
     public Sprite fishSprite;
+
+    /// <summary>
+    /// Déverrouille le poisson.
+    /// </summary>
+    public void UnlockFish()
+    {
+        if (!is_unlocked)
+        {
+            is_unlocked = true;
+            // Ajoute ici des actions supplémentaires, comme l'affichage d'une notification
+            Debug.Log($"Poisson '{fishName}' avec ID {fishID} a été déverrouillé !");
+        }
+    }
 }
