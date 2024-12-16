8e53a2e7f0283fe7cf4d35e19b1ebf21c6b51fde
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-12-16 10:49:34 +0100

    add sond fade out siteme

--- Assets/GameAssets/Gestion/Scripts/UI/UIbuttons.cs
+++ Assets/GameAssets/Gestion/Scripts/UI/UIbuttons.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class UIbuttons : MonoBehaviour
 {
-    [SerializeField] private GameObject unlockMenu ;
+        public AudioClip sfxClip;
+    public AudioClip musicClip;
+    [SerializeField] private GameObject unlockMenu;
     void Start()
     {
-        
-    }
+}
 
-public void UnlockMenuOpen(){
-        unlockMenu.SetActive(true);
+public void UnlockMenuOpen()
+{
+    SoundManager.instance.PlayMusic(musicClip);
+    SoundManager.instance.PlaySFX(sfxClip);
+    unlockMenu.SetActive(true);
 }
 }
