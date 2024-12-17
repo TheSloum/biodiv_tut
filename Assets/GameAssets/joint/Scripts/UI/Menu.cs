5ba9e31fcc1beeb748ac0b31a4e891e0f6d134fb
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2024-12-17 16:58:24 +0100

    add typo dyslexique

--- Assets/GameAssets/joint/Scripts/UI/Menu.cs
+++ Assets/GameAssets/joint/Scripts/UI/Menu.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Menu : MonoBehaviour
 {
-    public GameObject MainMenu;
-    public GameObject Parametre;
+    public GameObject MainMenu; // Référence au menu principal
+    public GameObject Parametre; // Référence aux paramètres
+
+    [Header("Font Changer Settings")]
+    public Toggle fontToggle; // La case à cocher (Toggle) pour changer de police
+    public TMP_FontAsset oldFont; // Police par défaut
+    public TMP_FontAsset newFont; // Nouvelle police
+    public TMP_Text[] textsToChange; // Liste des textes à changer
+
+    void Start()
+    {
+        // Attache l'écouteur pour le Toggle
+        if (fontToggle != null)
+            fontToggle.onValueChanged.AddListener(OnFontToggleChanged);
+    }
 
     void Update()
     {
@@ -21,6 +35,7 @@ public class Menu : MonoBehaviour
         }
     }
 
+    // Gestion des menus
     public void ShowOrHideElement()
     {
         MainMenu.SetActive(!MainMenu.activeSelf);
@@ -39,5 +54,17 @@ public class Menu : MonoBehaviour
         Parametre.SetActive(false);
         MainMenu.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    // Méthode appelée quand la case est cochée/décochée
+    void OnFontToggleChanged(bool isChecked)
+    {
+        foreach (TMP_Text txt in textsToChange)
+        {
+            if (txt != null)
+            {
+                txt.font = isChecked ? newFont : oldFont;
+            }
+        }
     }
 }
