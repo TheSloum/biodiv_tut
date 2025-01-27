d64d5994a72ea176b66acbd91d1ec97c634de5a8
Ninodev14 <115539009+Ninodev14@users.noreply.github.com>
2025-01-27 15:36:00 +0100

    add good typo

--- Assets/GameAssets/joint/Scripts/UI/HomeMenuHub.cs
+++ Assets/GameAssets/joint/Scripts/UI/HomeMenuHub.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
 
 public class HomeMenuHub : MonoBehaviour
 {
@@ -11,9 +13,11 @@ public class HomeMenuHub : MonoBehaviour
     public Button button4;
     public Button button5;
     public Button button6;
-    public Button button7;
 
-    // Start is called before the first frame update
+    public Color normalTextColor = Color.black;
+    public Color hoverTextColor = Color.blue;
+    public Color outlineColor = Color.white; 
+
     void Start()
     {
         button1.onClick.AddListener(Button1Clicked);
@@ -22,38 +26,56 @@ public class HomeMenuHub : MonoBehaviour
         button4.onClick.AddListener(Button4Clicked);
         button5.onClick.AddListener(Button5Clicked);
         button6.onClick.AddListener(Button6Clicked);
-        button7.onClick.AddListener(Button7Clicked);
+
+        AddHoverEffects(button1);
+        AddHoverEffects(button2);
+        AddHoverEffects(button3);
+        AddHoverEffects(button4);
+        AddHoverEffects(button5);
+        AddHoverEffects(button6);
     }
 
-    void Button1Clicked()
+    void Button1Clicked() { Debug.Log("Button 1 clicked!"); }
+    void Button2Clicked() { Debug.Log("Button 2 clicked!"); }
+    void Button3Clicked() { Debug.Log("Button 3 clicked!"); }
+    void Button4Clicked() { Debug.Log("Button 4 clicked!"); }
+    void Button5Clicked() { Debug.Log("Button 5 clicked!"); }
+    void Button6Clicked() { Application.Quit(); }
+    void AddHoverEffects(Button button)
     {
-        Debug.Log("Button 1 clicked!");
-    }
+        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+        if (buttonText == null)
+        {
+            Debug.LogWarning($"Le bouton {button.name} n'a pas de composant TextMeshProUGUI comme enfant !");
+            return;
+        }
 
-    void Button2Clicked()
-    {
-        Debug.Log("Button 2 clicked!");
-    }
+        EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
 
-    void Button3Clicked()
-    {
-        Debug.Log("Button 3 clicked!");
-    }
-    void Button4Clicked()
-    {
-        Debug.Log("Button 4 clicked!");
-    }
-    void Button5Clicked()
-    {
-        Debug.Log("Button 5 clicked!");
-    }
-    void Button6Clicked()
-    {
-        Debug.Log("Button 6 clicked!");
-    }
-    void Button7Clicked()
-    {
-        Application.Quit();
-    }
+        EventTrigger.Entry pointerEnter = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        pointerEnter.callback.AddListener((eventData) =>
+        {
+            buttonText.color = hoverTextColor;
 
+            buttonText.fontMaterial.SetFloat("_OutlineWidth", 0.2f);
+            buttonText.fontMaterial.SetColor("_OutlineColor", outlineColor);
+        });
+        trigger.triggers.Add(pointerEnter);
+
+        EventTrigger.Entry pointerExit = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+        pointerExit.callback.AddListener((eventData) =>
+        {
+
+            buttonText.color = normalTextColor;
+
+            buttonText.fontMaterial.SetFloat("_OutlineWidth", 0f);
+        });
+        trigger.triggers.Add(pointerExit);
+    }
 }
