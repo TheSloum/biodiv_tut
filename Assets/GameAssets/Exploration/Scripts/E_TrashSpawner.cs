048808a119049d7c962c21c402cd73c18bf9f246
Ninodev14 <nino.lefort@mmibordeaux.com>
2025-03-17 15:26:09 +0100

    add better aparition rock

--- Assets/GameAssets/Exploration/Scripts/E_TrashSpawner.cs
+++ Assets/GameAssets/Exploration/Scripts/E_TrashSpawner.cs
@@ -5,7 +5,7 @@ public class E_TrashSpawner : MonoBehaviour
     public static E_TrashSpawner Instance;
 
     [Header("Préfabriqué et paramètres de Trash")]
-    public GameObject trashPrefab; // Prefab du Trash à instancier
+    public GameObject[] trashPrefabs; // Tableau des trois prefabs à instancier
     public float spawnInterval = 3f; // Intervalle de spawn en secondes
     public float spawnRangeY = 3f;   // Plage verticale pour le spawn
     public float spawnXOffset = 10f; // Distance à droite de l'écran pour le spawn
@@ -33,7 +33,7 @@ public class E_TrashSpawner : MonoBehaviour
         mainCamera = Camera.main;
         if (mainCamera != null)
         {
-            // Calculer la position de l'arrière-plan droite de l'écran
+            // Calculer la position du bord droit de l'écran
             screenRightEdge = mainCamera.ViewportToWorldPoint(new Vector3(1, 0, mainCamera.nearClipPlane)).x;
         }
     }
@@ -50,18 +50,23 @@ public class E_TrashSpawner : MonoBehaviour
 
     void SpawnTrash()
     {
+        if (trashPrefabs.Length == 0) return; // Vérifier si la liste de prefabs n'est pas vide
+
+        // Sélectionner un prefab aléatoire
+        GameObject selectedTrash = trashPrefabs[Random.Range(0, trashPrefabs.Length)];
+
         // Calculer une position aléatoire sur l'axe Y dans la plage spécifiée
         float randomY = Random.Range(-spawnRangeY, spawnRangeY);
         // Position de spawn à droite de l'écran
         Vector3 spawnPosition = new Vector3(screenRightEdge + spawnXOffset, randomY, 0f);
-        // Instancier le Trash
-        Instantiate(trashPrefab, spawnPosition, Quaternion.identity);
+        // Instancier le Trash sélectionné
+        Instantiate(selectedTrash, spawnPosition, Quaternion.identity);
     }
 
     // Augmente le taux de spawn des Trash en diminuant l'intervalle
     public void IncreaseTrashSpawnRate()
     {
-        spawnInterval = defaultSpawnInterval / 2f; // Par exemple, la moitié de l'intervalle par défaut
+        spawnInterval = defaultSpawnInterval / 2f;
         Debug.Log("Vague de déchets activée : spawn des Trash augmenté.");
     }
 
@@ -70,7 +75,6 @@ public class E_TrashSpawner : MonoBehaviour
         spawnInterval = defaultSpawnInterval * 0.5f;
         Debug.Log("Vague de déchets activée : intervalle de spawn des trash diminué (spawn plus fréquent).");
     }
-
 
     // Restaure le taux de spawn des Trash à la valeur par défaut
     public void RestoreDefaultTrashSpawnRate()
